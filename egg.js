dbc82938d1b86581f32d345e2e24c70841dedc9b
Tanya Masvita <tanyamasvitaog@gmail.com>
2021-08-05 12:28:11 +0200

    Building the parser

--- egg.js
+++ egg.js
@@ -1,8 +1,21 @@
 function parseExpression(program) {
     program = skipSpace(program);
     let match, expr;
+    if (match = /^"([^"]*)"/.exec(program)) {
+        expr = {type: "value", value: match[1]};
+    } else if (match = /^\d+\b/.exec(program)) {
+        expr = {type: "value", value: Number(match[0])};
+    } else if (match = /^[^\s(), "]+/.exec(program)) {
+        expr = {type: "word", name: match[0]};
+    } else {
+        throw new SyntaxError("Unexpected Syntax: " + program);
+    }
+
+    // return parse apply here
 }
 
 function skipSpace(string) {
-
+    let first = string.search(/\S/);
+    if (first == -1) return "";
+    return string.slice(first);
 }
