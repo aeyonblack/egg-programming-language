afffe7cdddd2585cf11802295e3f57b3be688d72
Tanya Masvita <tanyamasvitaog@gmail.com>
2021-08-05 13:16:22 +0200

    Building the evaluator

--- egg.js
+++ egg.js
@@ -1,3 +1,5 @@
+// --- The Parser
+
 function parseExpression(program) {
     program = skipSpace(program);
     let match, expr;
@@ -49,4 +51,33 @@ function parse(program) {
     return expr;
 }
 
-console.log(parse("+(a,10)"));
+// --- The Evaluator
+
+const specialForms = Object.create(null);
+
+function evaluate(expr, scope) {
+    if (expr.type == "value") {
+        return expr.value;
+    } else if (expr.type == "word") {
+        if (expr.name in scope) {
+            return scope[expr.name];
+        } else {
+            throw new ReferenceError(
+                `Undefined binding: ${expr.name}`
+            );
+        }
+    } else if (expr.type == "apply") {
+        let {operator,args} = expr;
+        if (operator.type == "word" && 
+        operator.name in specialForms) {
+            return specialForms[operator.name](expr.args, scope);
+        } else {
+            let op = evaluate(operator, scope);
+            if (typeof op == "function") {
+                return op(...args.map(arg => evaluate(arg, scope)));
+            } else {
+                throw new TypeError("Applying a non-function");
+            }
+        }
+    }
+}
